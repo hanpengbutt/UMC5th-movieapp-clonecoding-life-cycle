e5bbf2344dcf3e7a6feebfd266dcd54319cf94c1
한지은 <hjn510450@gmail.com>
2023-11-10 09:47:58 +0900

    :lipstick:  design: update styles

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 function NotFound() {
-  return <div style={{ color: 'white' }}>올바르지 않은 경로입니다.</div>;
+  return (
+    <NoticeContainer>
+      <H1>해당 페이지를 찾지 못했습니다.</H1>
+      <div>주소가 잘못되었거나 더 이상 제공되지 않는 페이지입니다.</div>
+      <Link to='/'>
+        <MoveToMain>메인 페이지로 이동</MoveToMain>
+      </Link>
+    </NoticeContainer>
+  );
 }
 
 export default NotFound;
+
+const NoticeContainer = styled.div`
+  height: 80px;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: white;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Pretendard-SemiBold';
+  font-size: 24px;
+  line-hieght: 30px;
+`;
+
+const MoveToMain = styled.div`
+  color: tomato;
+`;
